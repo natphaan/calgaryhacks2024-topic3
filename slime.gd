479ac4ba1f9169ddd9b5c40811b440c6c5b6b004
Nathan Phan <nathan.phan@ucalgary.ca>
2024-02-17 21:57:06 -0700

    slime tracking and ai

--- slime.gd
+++ slime.gd
@@ -9,14 +9,15 @@ func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
 		
-		$AnimatedSprite2D.play("play")
+		$AnimatedSprite2D.play("walk")
 	else:
 		$AnimatedSprite2D.play("idle")
 
-func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
 
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
